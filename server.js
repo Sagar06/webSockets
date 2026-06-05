d514415aa4766dd40e7325ea859ba30f1d2a1b56
Sagar06 <sagund2000@gmail.com>
2026-06-05 23:39:25 +0530

    Initialize WebSocket server with HTML frontend and necessary configuration files

---
+++ server.js
@@ -0,0 +1,31 @@
+//build in module
+import http from "node:http";
+//tp read HTML
+import fs from "node:fs/promises";
+import path from "path";
+import { WebSocketServer } from "ws";
+
+const PORT = process.env.PORT ?? 9000;
+
+//create http server
+const httpServer = http.createServer(async function (req, res) {
+  const indexFile = await fs.readFile(path.resolve("./index.html"), "utf-8");
+  res.setHeader("Content-Type", "text/html");
+  return res.end(indexFile); //read html file and send to frontend
+});
+
+const webServer = new WebSocketServer({ server: httpServer }); //webSocket server
+
+//frontend send upgrade request, it will handle
+webServer.on("connection", (websocket) => {
+  console.log(`WebSocket Connection connected!`);
+  //print when FT sends some message
+  websocket.on("message", (data) => {
+    console.log(`WebSocket Message Recieved `, data.toString());
+  });
+});
+
+//httpserver listen
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
